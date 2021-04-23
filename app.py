cdd5edf05203e0cdc91cb608f1ed7d302aa51b8b
NarbehJackson <81082128+NarbehJackson@users.noreply.github.com>
2021-04-23 21:44:54 +0430

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():    
-    return 'Welcome to the Application with updated code!'
+    return 'Welcome to the MAMAD with updated code!'
 
 @app.route('/stub')
 def stub():
