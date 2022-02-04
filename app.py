ce5a019cd557bb1dc5a8a4eb6e6ec1cc6600ba94
NarbehJackson <81082128+NarbehJackson@users.noreply.github.com>
2022-02-04 15:00:37 +0330

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():    
-    return 'Welcome to the jascvvem with updated code!'
+    return 'Welcome to the jascvvccem with updated code!'
 
 @app.route('/stub')
 def stub():
